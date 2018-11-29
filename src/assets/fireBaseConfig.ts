3818275d90a29b971541fbb8262185d3611811c0
MONSTERLAP\ego <herman.monster@noreply.github.com>
2018-11-29 14:01:20 +0100

    ..

---
+++ src/assets/fireBaseConfig.ts
@@ -0,0 +1,13 @@
+// Goo Seite zur datenbetrachtung/änderung
+// https://console.firebase.google.com/project/kanbantodo/database/firestore/data~2Fmykanbanbacklog~2F1
+
+// Config für KanbanData
+
+export  const firebaseConfig = {
+    apiKey: "AIzaSyD2PfRu3hoXryEEvPEYw7VLr5o-0n9XHos",
+    authDomain: "kanbantodo.firebaseapp.com",
+    databaseURL: "https://kanbantodo.firebaseio.com",
+    projectId: "kanbantodo",
+    storageBucket: "kanbantodo.appspot.com",
+    messagingSenderId: "911378615179"
+  };
